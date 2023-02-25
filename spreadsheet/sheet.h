a76cc1c0b1b0dd1a22f89acd44ad1b7a259d40ef
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2023-02-25 18:18:35 +0300

    Update sheet.h

--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,41 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+
+#include <functional>
+#include <unordered_map>
+
+class Cell;
+
+struct Hasher
+{
+    size_t operator()(Position pos) const;
+};
+
+class Sheet : public SheetInterface
+{
+public:
+    ~Sheet() override;
+
+    void SetCell(Position pos, const std::string& text) override;
+
+    const CellInterface* GetCell(Position pos) const override;
+    CellInterface* GetCell(Position pos) override;
+
+    void ClearCell(Position pos) override;
+
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+
+    void ResizeTable();
+    bool CheckCircularDependency(Position pos, const std::vector<Position>& dep_cells);
+
+private:
+    void CheckPosition(Position pos) const;
+
+    Size table_size_= {0, 0};
+    std::unordered_map<Position, std::unique_ptr<Cell>, Hasher> table_;
+};
