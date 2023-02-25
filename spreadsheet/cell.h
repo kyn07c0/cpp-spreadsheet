d27fc4eedf13b131b5934c30f33ec031c33f3f25
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2023-02-25 18:08:32 +0300

    Update cell.h

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -2,35 +2,83 @@
 
 #include "common.h"
 #include "formula.h"
+#include "sheet.h"
 
 #include <functional>
 #include <unordered_set>
+#include <set>
 
 class Sheet;
 
-class Cell : public CellInterface {
+class Impl
+{
 public:
-    Cell(Sheet& sheet);
-    ~Cell();
+    virtual CellInterface::Value GetValue() = 0;
+    virtual std::string GetText() const = 0;
+    virtual std::vector<Position> GetReferencedCells() const = 0;
+};
 
-    void Set(std::string text);
+class EmptyImpl : public Impl
+{
+public:
+    EmptyImpl();
+
+    CellInterface::Value GetValue() override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+};
+
+class TextImpl : public Impl
+{
+public:
+    explicit TextImpl(std::string text);
+
+    CellInterface::Value GetValue() override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+
+private:
+    std::string text_;
+};
+
+class FormulaImpl : public Impl
+{
+public:
+    FormulaImpl(std::string text, SheetInterface& sheet);
+
+    CellInterface::Value GetValue() override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+
+private:
+    std::unique_ptr<FormulaInterface> formula_;
+    SheetInterface& sheet_;
+    CellInterface::Value cache_;
+};
+
+
+class Cell : public CellInterface
+{
+public:
+    explicit Cell(SheetInterface& sheet);
+    ~Cell() override;
+
+    void Set(const std::string& text);
     void Clear();
 
     Value GetValue() const override;
     std::string GetText() const override;
+
     std::vector<Position> GetReferencedCells() const override;
 
-    bool IsReferenced() const;
+    void AddDependentCell(Position pos);
+    void DeleteDependentCell(Position pos);
+
+    void CacheInvalidation();
+    void CacheUpdate();
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
-
-    std::unique_ptr<Impl> impl_;
-
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
-
+    std::unique_ptr<Impl> impl_ = nullptr;
+    SheetInterface& sheet_;
+    std::set<Position> dependent_cells_;
 };
