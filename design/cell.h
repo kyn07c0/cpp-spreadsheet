c2755986a38278af503bafa9b24bc33605851d9b
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2023-02-25 18:00:34 +0300

    Add files via upload

--- design/cell.h
+++ design/cell.h
@@ -2,22 +2,82 @@
 
 #include "common.h"
 #include "formula.h"
+#include "sheet.h"
 
-class Cell : public CellInterface {
+#include <functional>
+#include <unordered_set>
+
+class Sheet;
+
+class Impl
+{
 public:
-    Cell();
+    virtual CellInterface::Value GetValue() = 0;
+    virtual std::string GetText() const = 0;
+    virtual const std::vector<Position>& GetReferencedCells() const = 0;
+};
+
+class EmptyImpl : public Impl
+{
+public:
+    EmptyImpl();
+
+    CellInterface::Value GetValue() override;
+    std::string GetText() const override;
+    const std::vector<Position>& GetReferencedCells() const override;
+};
+
+class TextImpl : public Impl
+{
+public:
+    TextImpl(std::string text);
+
+    CellInterface::Value GetValue() override;
+    std::string GetText() const override;
+    const std::vector<Position>& GetReferencedCells() const override;
+
+private:
+    std::string text_;
+};
+
+class FormulaImpl : public Impl
+{
+public:
+    FormulaImpl(std::string text, SheetInterface& sheet);
+
+    CellInterface::Value GetValue() override;
+    std::string GetText() const override;
+    const std::vector<Position>& GetReferencedCells() const override;
+
+private:
+    std::unique_ptr<FormulaInterface> formula_;
+    SheetInterface& sheet_;
+    CellInterface::Value cache_;
+};
+
+
+class Cell : public CellInterface
+{
+public:
+    Cell(SheetInterface& sheet);
     ~Cell();
 
-    void Set(std::string text);
+    void Set(const std::string& text);
     void Clear();
 
     Value GetValue() const override;
     std::string GetText() const override;
 
+    std::vector<Position> GetReferencedCells() const override; // не удалось установить константную ссылку, т.к. переопределяется метод интерфейса CellInterface, которы нельзя менять
+    
+    void AddDependentCell(Position pos);
+    void DeleteDependentCell(Position pos);
+    
+    void CacheInvalidation();
+    void CacheUpdate();
+
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
-    std::unique_ptr<Impl> impl_;
+    std::unique_ptr<Impl> impl_ = nullptr;
+    SheetInterface& sheet_;
+    std::vector<Position> dependent_cells_;
 };
